5b70269e7365e01ece3119005a459ca722657610
enoshima7 <duanjipeng0328@gmail.com>
2020-06-22 16:48:08 +0800

    build: 后台框架搭建

---
+++ server/apps/admin/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Woke后台管理API')
+    .setDescription('后台管理页面调用的API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
+  console.log('http://localhost:3000/api-docs');
+}
+bootstrap();
